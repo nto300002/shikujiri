8831a19200b56414da2e54b78740545ecb5ed119
nto300002 <samonkntd@gmail.com>
2023-10-02 23:39:54 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     3年B組! しくじり先生 
+    </div>
+  );
+}
+
+export default App;
